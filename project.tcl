7f5bdca190944955c6336c548009afacea246bdd
Ravi G Ravitharan <ravitharan@gmail.com>
2025-04-03 13:58:45 +0530

    Single project script for all modules

---
+++ project.tcl
@@ -0,0 +1,32 @@
+set ALL_PROJECTS(0) {compare v v}
+set ALL_PROJECTS(1) {compare vhd vhd}
+set ALL_PROJECTS(2) {incdec v v}
+set ALL_PROJECTS(3) {incdec vhd vhd}
+set ALL_PROJECTS(4) {lookup v v}
+set ALL_PROJECTS(5) {lookup vhd vhd}
+set ALL_PROJECTS(6) {multiply v v}
+set ALL_PROJECTS(7) {multiply vhd vhd}
+set ALL_PROJECTS(8) {lut_ram v v}
+
+set PROJECT [lindex $ALL_PROJECTS($inx) 0]
+set EXT [lindex $ALL_PROJECTS($inx) 1]
+set TB_EXT [lindex $ALL_PROJECTS($inx) 2]
+
+if {$EXT == "v"} {
+  set LANGUAGE verilog
+} elseif {$EXT == "vhd"} {
+  set LANGUAGE vhdl
+}
+
+set SRC ./$LANGUAGE/${PROJECT}.${EXT}
+set TB_SRC ./$LANGUAGE/tb_${PROJECT}.${TB_EXT}
+
+create_project -force ${PROJECT} ./${PROJECT}_${EXT} -part xck26-sfvc784-2LVI-i
+set_property board_part xilinx.com:k26i:part0:1.4 [current_project]
+set_property target_language $LANGUAGE [current_project]
+
+add_files -norecurse ${SRC}
+add_files -fileset sim_1 -norecurse ${TB_SRC}
+
+launch_runs impl_1 -jobs 16
+
