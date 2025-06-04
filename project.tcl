62746bde62436075c6043106ac26c8d744cc87c2
Ravi G Ravitharan <ravitharan@gmail.com>
2025-06-04 12:24:57 +0100

    Add separate block and non block

--- project.tcl
+++ project.tcl
@@ -10,6 +10,8 @@ set ALL_PROJECTS(8) {lut_ram v v}
 set ALL_PROJECTS(9) {compare_2 v v}
 set ALL_PROJECTS(10) {compare_3 v v}
 set ALL_PROJECTS(11) {edge sv sv}
+set ALL_PROJECTS(12) {edge_nb sv sv}
+set ALL_PROJECTS(13) {edge_b sv sv}
 
 set PROJECT [lindex $ALL_PROJECTS($inx) 0]
 set EXT [lindex $ALL_PROJECTS($inx) 1]
---
+++ systemverilog/edge_b/tb_edge_b.sv
@@ -0,0 +1,20 @@
+`timescale 1ns/1ps
+
+module tb_edge_b();
+
+  reg clk;
+  reg nReset;
+  reg out_edge;
+
+  edge_b edge_1(clk, nReset, out_edge);
+
+  initial
+    begin
+      clk = 0;
+      nReset = 0;
+      #100 nReset = 1;
+    end
+
+  always #5 clk = ~clk;
+
+endmodule
