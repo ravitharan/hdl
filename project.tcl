d607a741ff17e342a3b5e7eeb6802f0297517c92
Ravi G Ravitharan <ravitharan@gmail.com>
2025-06-04 09:04:59 +0100

    Add SV edge detector

--- project.tcl
+++ project.tcl
@@ -9,6 +9,7 @@ set ALL_PROJECTS(7) {multiply vhd vhd}
 set ALL_PROJECTS(8) {lut_ram v v}
 set ALL_PROJECTS(9) {compare_2 v v}
 set ALL_PROJECTS(10) {compare_3 v v}
+set ALL_PROJECTS(11) {edge sv sv}
 
 set PROJECT [lindex $ALL_PROJECTS($inx) 0]
 set EXT [lindex $ALL_PROJECTS($inx) 1]
@@ -18,6 +19,8 @@ if {$EXT == "v"} {
   set LANGUAGE verilog
 } elseif {$EXT == "vhd"} {
   set LANGUAGE vhdl
+} elseif {$EXT == "sv"} {
+  set LANGUAGE systemverilog
 }
 
 set SRC ./$LANGUAGE/$PROJECT/$PROJECT.$EXT
@@ -25,7 +28,9 @@ set TB_SRC ./$LANGUAGE/$PROJECT/tb_$PROJECT.$TB_EXT
 
 create_project -force ${PROJECT} ./$LANGUAGE/$PROJECT/$PROJECT -part xck26-sfvc784-2LVI-i
 set_property board_part xilinx.com:k26i:part0:1.4 [current_project]
-set_property target_language $LANGUAGE [current_project]
+if {$LANGUAGE != "systemverilog"} {
+  set_property target_language $LANGUAGE [current_project]
+}
 
 add_files -norecurse ${SRC}
 add_files -fileset sim_1 -norecurse ${TB_SRC}
---
+++ systemverilog/edge/edge.sv
@@ -0,0 +1,83 @@
+`define BLOCKING_ASSIGNMENT     // For clock generation
+`define NON_BLOCK_TRUST_DELAY   // edge generation
+
+module edge_detector(i_clk, i_nReset, o_edge);
+
+    parameter WIDTH = 4;
+
+    input i_clk;
+    input i_nReset;
+    output logic o_edge;
+
+    localparam logic [WIDTH-1:0] LIMITS[1:0] = {12, 8};
+
+    logic window, window_last;
+
+`ifndef BLOCKING_ASSIGNMENT
+    always_ff @(posedge i_clk) begin : WINDOW_GENERATOR_NON_BLOCKING
+        logic [WIDTH-1:0] count;
+        if (!i_nReset) begin
+            window <= 0;
+            count <= LIMITS[0];
+        end else begin
+            if (count == 0) begin
+                count <= LIMITS[~window] - 1;
+                window <= ~window;
+            end else begin
+                count <= count - 1;
+            end
+        end
+    end
+`else
+    always_ff @(posedge i_clk) begin : WINDOW_GENERATOR_BLOCKING
+        logic [WIDTH-1:0] count;
+        if (!i_nReset) begin
+            window = 0;
+            count = LIMITS[0];
+        end else begin
+            if (count == 0) begin
+                count = LIMITS[~window] - 1;
+                window = ~window;
+            end else begin
+                count = count - 1;
+            end
+        end
+    end
+`endif
+
+`ifdef NON_BLOCK_TRUST_DELAY
+
+    always_ff @(posedge i_clk) begin
+        if (!i_nReset) begin
+            window_last <= 0;
+        end else begin
+            window_last <= window;
+        end
+    end
+    assign o_edge = window_last ^ window;
+
+`elsif NON_BLOCK_NO_TRUST_DELAY
+
+    always_ff @(posedge i_clk) begin
+        if (!i_nReset) begin
+            window_last <= 0;
+        end else begin
+            o_edge <= window_last ^ window;
+            window_last <= window;
+        end
+    end
+
+`elsif BLOCK_NO_TRUST_DELAY
+
+    always_ff @(posedge i_clk) begin
+        if (!i_nReset) begin
+            window_last = 0;
+        end else begin
+            o_edge = window_last ^ window;
+            window_last = window;
+        end
+    end
+
+`endif
+
+endmodule
